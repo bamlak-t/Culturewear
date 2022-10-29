bb48196c74063c49901049c5063bced405535485
Samuel Adekunle <ebnsamuel@gmail.com>
2022-10-29 13:57:35 +0100

    add theme provider

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,20 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import Head from 'next/head'
+import theme from '../src/theme'
 
 function MyApp({ Component, pageProps }) {
   return <>
     <Head>
       <meta name="viewport" content="initial-scale=1, width=device-width" />
     </Head>
-    <CssBaseline />
-    <Component {...pageProps} />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+
   </>
 
 
