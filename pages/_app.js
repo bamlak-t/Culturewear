e16170788177043f7154b7954d133b577bed48d8
Samuel Adekunle <ebnsamuel@gmail.com>
2022-10-29 13:53:52 +0100

    added css baseline

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,19 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { CssBaseline } from '@mui/material';
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+    </Head>
+    <CssBaseline />
+    <Component {...pageProps} />
+  </>
+
+
 }
 
 export default MyApp
