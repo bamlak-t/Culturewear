5f7881ff75e57af91fffe133ab93ceb0f096e658
Samuel Adekunle <ebnsamuel@gmail.com>
2022-10-29 13:42:43 +0100

    update

--- src/Copyright.js
+++ src/Copyright.js
@@ -6,7 +6,7 @@ export default function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
       {'Copyright © '}
-      <MuiLi  nk color="inherit" href="https://mui.com/">
+      <MuiLink color="inherit" href="https://mui.com/">
         Your Website
       </MuiLink>{' '}
       {new Date().getFullYear()}.
