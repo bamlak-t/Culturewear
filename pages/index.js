041bba04ab42231e9b25881eec27225e9df5c221
Samuel Adekunle <ebnsamuel@gmail.com>
2022-10-29 17:57:34 +0100

    fetch designs in home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
+import axios from "axios"
+import Link from "next/link";
+import { useEffect, useState } from "react"
+
 export default function Home() {
+  const [designs, setDesigns] = useState([]);
+
+  const getDesigns = async () => {
+    const res = await axios.get('/api/designs')
+    setDesigns(res.data.designsData)
+  }
+
+  useEffect(() => { getDesigns() }, [])
+
   return (
     <>
-      <a href="/api/auth/login">Login</a>
+      <Link href="/api/auth/login">Login</Link>
       <br />
-      <a href="/api/auth/logout">Logout</a>
+      <Link href="/api/auth/logout">Logout</Link>
+      <br />
+      <p>Desgins: {String(designs.map(design => design.name))} </p>
     </>
   )
 }
